472e2bda57acb87045828c7b608719342316f888
JuanMPaola <juanmpaola25@gmail.com>
2024-05-24 16:07:19 -0300

    Merge form and main

--- src/app/form/categories/hooks/useFormCategories.js
+++ src/app/form/categories/hooks/useFormCategories.js
@@ -1,43 +1,41 @@
-
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories, postCategories, deleteCategories } from '../../../redux/actions';
 
 const useFormCategories = () => {
     const [categorias, setCategorias] = useState("");
-    const [listCategories, setListCategories] = useState([])
+    const [listCategories, setListCategories] = useState([]);
     const dispatch = useDispatch();
-    const categoriaState = useSelector(state => state.firebase.categories)
-  
-    useEffect(() =>{
-        dispatch(getCategories())
-    },[])
-    useEffect(() =>{
-      const hasSemeData = categoriaState.length === listCategories.length;
-      if(!hasSemeData){
-          setListCategories(categoriaState);
-      }
-  },[categoriaState, listCategories]);
+    const categoriaState = useSelector(state => state.firebase.categories);
 
-    const handleSumbit = (e) => {
-      e.preventDefault();
-      console.log(" Test Handle Sumbmit", categorias);
-      dispatch(postCategories(categorias))
-      setCategorias("");
-    }
-  
-    const handleChange = (e) =>{
-      setCategorias(e.target.value)
-    }
-  
-    const handleDelete = (id) =>{
-      console.log("Aca esta el ", id);
-      dispatch(deleteCategories(id))
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch]);
+
+    useEffect(() => {
+        const hasSameData = categoriaState.length === listCategories.length;
+        if (!hasSameData) {
+            setListCategories(categoriaState);
+        }
+    }, [categoriaState, listCategories]);
+
+    const handleSubmit = (e) => {  // Corrige el nombre aquí
+        e.preventDefault();
+        console.log("Test Handle Submit", categorias);
+        dispatch(postCategories(categorias));
+        setCategorias("");
     }
 
+    const handleChange = (e) => {
+        setCategorias(e.target.value);
+    }
 
-    return {categorias, handleChange, handleSumbit, categoriaState, handleDelete, listCategories}
+    const handleDelete = (id) => {
+        console.log("Aca esta el ", id);
+        dispatch(deleteCategories(id));
+    }
+
+    return { categorias, handleChange, handleSubmit, categoriaState, handleDelete, listCategories };
 }
 
-
-export default useFormCategories
+export default useFormCategories;
