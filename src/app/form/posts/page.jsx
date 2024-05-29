19f05c8c9233b75e920c2dfef47332305d99edbc
JuanMPaola <juanmpaola25@gmail.com>
2024-05-29 17:46:40 -0300

    mejorando formulario no andan los errores

--- src/app/form/posts/page.jsx
+++ src/app/form/posts/page.jsx
@@ -10,20 +10,20 @@ import { uploadFile } from '@/src/firebase'
 
 function PostForm() {
   const { categoriaState } = useFormCategories()
-  console.log("categorias desde post", categoriaState)
   const posteos = useSelector(state => state.firebase.posteos)
   const marcas = useSelector(state => state.firebase.marcas)
-  console.log(marcas, "Aca estan las marcas")
 
   const [postState, setPostState] = useState({
     available: true,
     marca: "",
     categories: "",
     description: "",
-    images: [], 
+    images: [],
     title: "",
   });
-  
+
+  const [errors, setErrors] = useState([])
+
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -35,7 +35,22 @@ function PostForm() {
   }
 
   const handleSubmit = (e) => {
+    setErrors([]);
+    const validationErros = [];
+
+    if (postState.marca.length < 1) validationErros.push("Seleccione una marca.")
+    
+    if (postState.title.length < 1) validationErros.push("Agregue el titulo de la publicación.")
+
+    if(postState.images.length < 1) validationErros.push("No hay imagenes")
+
+    if (validationErros.length > 0) {
+      setErrors(validationErros)
+      return;
+    }
+
     e.preventDefault();
+
     dispatch(postPosts(postState))
   }
 
@@ -69,7 +84,7 @@ function PostForm() {
   return (
     <div className={style.container}>
       <div className={style.containerForm}>
-      <h2>Form Posts</h2>
+        <h2>Form Posts</h2>
         <form onSubmit={handleSubmit} className={style.form}>
           <div>
             <label htmlFor="">Titulo</label>
@@ -100,6 +115,11 @@ function PostForm() {
           <div>
             <label htmlFor="">Imagenes</label>
             <input type="file" multiple onChange={e => handleImages(e.target.files)} />
+            <div>
+              {postState.images.length > 0 ? postState.images.map((image, index) => (
+                <img key={index} src={image} alt={`upload-${index}`} style={{ width: '100px', height: '100px', objectFit: 'cover', margin: '5px' }} />
+              )) : <p>No hay imágenes seleccionadas</p>}
+            </div>
           </div>
           <div className={style.btnPublicar}>
             <button type='submit' className={style.publicar}>Publicar</button>
@@ -107,22 +127,24 @@ function PostForm() {
         </form>
       </div>
       <div className={style.containerCards}>
-{/*           <Posts/> */}
-      <h2>Posteos</h2>
-          <ul>
-              {posteos.map(posteo => (
-                <li className={style.card} key={posteo.id}>
-                  <button onClick={() => handleDelete(posteo.id)}>X</button>
-                  <img src={posteo.imagenes/* [0] */} alt="" className={style.cardImage} />
-                  <Link href="/posts/[id]" as={`/posts/${posteo.id}`} key={posteo.id}>
-                    <p className={style.cardTitle}>{posteo.id}</p>
-                  </Link>
-                  <p className={style.cardDes}>{posteo.descripcion}</p>
-                  <p>{posteo.disponible}</p>
-                  <p>{posteo.categoria}</p>
-                </li>
-              ))}
-            </ul>
+        {/*           <Posts/> */}
+        <h2>Posteos</h2>
+        <ul>
+          {posteos.map(posteo => (
+            <li className={style.card} key={posteo.id}>
+              <div className={style.divEliminar}>
+                <p onClick={() => handleDelete(posteo.id)} className={style.eliminar}>Eliminar</p>
+              </div>
+              <img src={posteo.imagenes/* [0] */} alt="" className={style.cardImage} />
+              <Link href="/posts/[id]" as={`/posts/${posteo.id}`} key={posteo.id}>
+                <p className={style.cardTitle}>{posteo.id}</p>
+              </Link>
+              <p className={style.cardDes}>{posteo.descripcion}</p>
+              <p>{posteo.disponible}</p>
+              <p>{posteo.categoria}</p>
+            </li>
+          ))}
+        </ul>
 
       </div>
     </div>
